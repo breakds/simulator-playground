25acf1dca9397a8101fcd5b7d8244fbd2a1d0c5b
Break Yang <breakds@gmail.com>
2022-12-14 14:32:06 -0800

    Add hello world example

---
+++ experiments/pybullet/hello_world.py
@@ -0,0 +1,22 @@
+import pybullet as p
+import time
+import pybullet_data
+
+# p.GUI for graphical, or p.DIRECT for headless
+client = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
+p.setGravity(0, 0, -10)
+
+planeId = p.loadURDF("plane.urdf")
+startPos = [0, 0, 1]
+startOrientation = p.getQuaternionFromEuler([0, 0, 0])
+
+boxId = p.loadURDF("r2d2.urdf", startPos, startOrientation)
+# set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
+for i in range(10000):
+    p.stepSimulation()
+    time.sleep(1.0 / 240.0)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print(cubePos, cubeOrn)
+p.disconnect()
