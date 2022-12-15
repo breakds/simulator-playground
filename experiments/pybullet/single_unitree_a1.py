ed639f7dc1fe6a57960e6247f0ccb9eca022ccb3
Break Yang <breakds@gmail.com>
2022-12-14 17:01:19 -0800

    Update single unitree a1

--- experiments/pybullet/single_unitree_a1.py
+++ experiments/pybullet/single_unitree_a1.py
@@ -1,7 +1,31 @@
+import pkgutil
 import numpy as np
 import pybullet as p
 import pybullet_data
 import time
+from PIL import Image
+
+
+class Perf(object):
+    def __init__(self, name):
+        self._name = name
+        self._n = 0
+        self._t = 0.0
+        self._ts = 0.0
+
+    def start(self):
+        self._ts = time.perf_counter()
+
+    def stop(self):
+        end = time.perf_counter()
+        self._t += end - self._ts
+        self._n += 1
+
+    def report(self):
+        print(
+            f"{self._name}: N = {self._n}, Total = {self._t:.3f}, "
+            f"Average = {self._t / self._n:.10f}"
+        )
 
 
 def inspect_robot(robot):
@@ -14,7 +38,13 @@ def inspect_robot(robot):
         )
 
 
-p.connect(p.GUI)
+p.connect(p.DIRECT)
+egl = pkgutil.get_loader("eglRenderer")
+plugin = p.loadPlugin(egl.get_filename(), "_eglRendererPlugin")
+p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+
+
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0.0, 0.0, -9.8)
 p.setRealTimeSimulation(0)
@@ -32,19 +62,67 @@ a1 = p.loadURDF(
 )
 inspect_robot(a1)
 
+projection_matrix = [
+    1.0825318098068237,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    1.732050895690918,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    -1.0002000331878662,
+    -1.0,
+    0.0,
+    0.0,
+    -0.020002000033855438,
+    0.0,
+]
 
-for setp in range(3000):
+perf_physics = Perf("Physics")
+perf_render = Perf("Render")
+
+img = None
+camera_distance = 1.0
+
+for step in range(1000):
     target_positions = np.random.uniform(-0.8, 0.8, size=(4,))
     p.setJointMotorControlArray(
         a1, [3, 8, 13, 18], p.POSITION_CONTROL, targetPositions=target_positions
     )
 
-    focus_position, _ = p.getBasePositionAndOrientation(a1)
-    p.resetDebugVisualizerCamera(
-        cameraDistance=3,
-        cameraYaw=0,
-        cameraPitch=-40,
-        cameraTargetPosition=focus_position,
-    )
+    perf_physics.start()
     p.stepSimulation()
-    time.sleep(0.01)
+    perf_physics.stop()
+
+    focus_position, _ = p.getBasePositionAndOrientation(a1)
+    view_matrix = p.computeViewMatrixFromYawPitchRoll(
+        focus_position,
+        camera_distance,  # distance
+        0.0,  # yaw
+        -40.0,  # pitch
+        0.0,  # roll
+        2,
+    )  # up axis index (2 means z direction)
+
+    perf_render.start()
+    img = p.getCameraImage(
+        600,
+        400,
+        viewMatrix=view_matrix,
+        projectionMatrix=projection_matrix,
+        shadow=1,
+        lightDirection=[1, 1, 1],
+    )
+    perf_render.stop()
+
+    im = Image.fromarray(img[2][:, :, :3])
+    im.save(f"/home/breakds/tmp/bullet_images_{step}.jpg")
+
+print(f"Rendering image shape: {img[2].shape}")
+perf_physics.report()
+perf_render.report()
+
+p.unloadPlugin(plugin)
