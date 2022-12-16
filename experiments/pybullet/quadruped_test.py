e44a6c8d93e8dcab6de8137574310a7f0ceb1872
Break Yang <breakds@gmail.com>
2022-12-15 17:24:24 -0800

    Add multi processing support

--- experiments/pybullet/quadruped_test.py
+++ experiments/pybullet/quadruped_test.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pybullet as p
 import pybullet_data
+from pybullet_utils import bullet_client as bc
 import pkgutil
 import time
 from PIL import Image
 
 
+NUM_ENVS = 20
+NUM_AGENTS = 1
 HEADLESS = True
 GROUND_RADIUS = 2.0
 
@@ -57,17 +60,19 @@ projection_matrix = np.array(
 class QuadrupedEnvironment(object):
     def __init__(self, action_freq=10):
         if HEADLESS:
-            p.connect(p.DIRECT)
+            self._client = bc.BulletClient(connection_mode=p.DIRECT)
             egl = pkgutil.get_loader("eglRenderer")
-            self._plugin = p.loadPlugin(egl.get_filename(), "_eglRendererPlugin")
-            p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
-            p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+            self._plugin = self._client.loadPlugin(
+                egl.get_filename(), "_eglRendererPlugin"
+            )
+            self._client.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
+            self._client.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
         else:
-            p.connect(p.GUI)
-        p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        p.setGravity(0.0, 0.0, -9.8)
+            self._client.connect(p.GUI)
+        self._client.setAdditionalSearchPath(pybullet_data.getDataPath())
+        self._client.setGravity(0.0, 0.0, -9.8)
 
-        self._ground = p.loadURDF(
+        self._ground = self._client.loadURDF(
             "plane.urdf", [0.0, 0.0, 0.0], flags=p.URDF_ENABLE_CACHED_GRAPHICS_SHAPES
         )
         self._robots = []
@@ -78,7 +83,8 @@ class QuadrupedEnvironment(object):
 
     def __del__(self):
         if HEADLESS:
-            p.unloadPlugin(self._plugin)
+            self._client.unloadPlugin(self._plugin)
+            self._client.disconnect()
 
     @property
     def n(self):
@@ -90,37 +96,37 @@ class QuadrupedEnvironment(object):
         # Pybullet by default runs at 240 Hz, i.e. each physical step = 1 / 240
         # second in logical time.
         for i in range(self.n):
-            p.setJointMotorControlArray(
+            self._client.setJointMotorControlArray(
                 self._robots[i],
-                np.arange(p.getNumJoints(self._robots[i])),
+                np.arange(self._client.getNumJoints(self._robots[i])),
                 p.POSITION_CONTROL,
                 targetPositions=target_positions[i],
             )
 
         for _ in range(self._steps_per_action):
-            p.stepSimulation()
+            self._client.stepSimulation()
 
-        view_matrix = p.computeViewMatrixFromYawPitchRoll(
-            [0.01, -0.01, 0.4],
-            2.0,  # distance
-            0.0,  # yaw
-            -40.0,  # pitch
-            0.0,  # roll
-            2,
-        )  # up axis index (2 means z direction)
-        snapshot = p.getCameraImage(
-            600,
-            400,
-            viewMatrix=view_matrix,
-            projectionMatrix=projection_matrix,
-            shadow=1,
-            lightDirection=[1, 1, 1],
-        )
+        for i in range(self.n):
+            view_matrix = self._client.computeViewMatrixFromYawPitchRoll(
+                p.getBasePositionAndOrientation(self._robots[i])[0],
+                2.0,  # distance
+                0.0,  # yaw
+                -40.0,  # pitch
+                0.0,  # roll
+                2,
+            )  # up axis index (2 means z direction)
+
+            self._client.getCameraImage(
+                600,
+                400,
+                viewMatrix=view_matrix,
+                projectionMatrix=projection_matrix,
+                shadow=1,
+                lightDirection=[1, 1, 1],
+            )
+
         self._perf_step.stop()
         self._steps_count += 1
-        Image.fromarray(snapshot[2][:, :, :3]).save(
-            f"/home/breakds/tmp/bullet_capture/p{self._steps_count}.jpg"
-        )
 
     def reset(self, n: int = 10):
         self._perf_reset.start()
@@ -134,7 +140,7 @@ class QuadrupedEnvironment(object):
 
         for i in range(n):
             self._robots.append(
-                p.loadURDF(
+                self._client.loadURDF(
                     "a1/a1.urdf",
                     positions[i],
                     flags=p.URDF_ENABLE_CACHED_GRAPHICS_SHAPES,
@@ -145,7 +151,7 @@ class QuadrupedEnvironment(object):
     def run(self, steps: int = 100):
         for _ in range(steps):
             target_positions = np.random.uniform(
-                -0.2, 0.5, size=(self.n, p.getNumJoints(self._robots[0]))
+                -0.2, 0.5, size=(self.n, self._client.getNumJoints(self._robots[0]))
             )
             self.step(target_positions)
             if not HEADLESS:
@@ -156,7 +162,31 @@ class QuadrupedEnvironment(object):
         self._perf_step.report()
 
 
-env = QuadrupedEnvironment()
-env.reset(1)
-env.run(1000)
-env.report()
+def run_single_env(signal):
+    env = QuadrupedEnvironment()
+    env.reset(NUM_AGENTS)
+    for _ in range(1000):
+        signal.wait()
+        target_positions = np.random.uniform(
+            -0.2, 0.5, size=(env.n, env._client.getNumJoints(env._robots[0]))
+        )
+        env.step(target_positions)
+    return env._perf_step
+
+
+if __name__ == "__main__":
+    from multiprocessing import Pool, Manager
+
+    manager = Manager()
+
+    with Pool(NUM_ENVS) as pool:
+        signal = manager.Event()
+        results = pool.map_async(run_single_env, [signal] * NUM_ENVS)
+        for _ in range(1000):
+            signal.set()
+        n = 0
+        t = 0.0
+        for result in results.get():
+            n += result._n
+            t += result._t
+        print(f"Average {t / n} seconds per step. n = {n}.")
